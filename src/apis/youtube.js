c7369c5061a720a078b4db4b33e5ec796eedb2b4
scottdavenport <scott@pinpoint.com>
2021-01-04 18:30:43 -0500

    Making a request to youtube

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyDzaXTBGkePTgPcUrInKtWiL0PS-JFkf10';
+
+export default axios.create({
+	baseURL: 'https://www.googleapis.com/youtube/v3',
+	params: {
+		part: 'snippet',
+		type: 'video',
+		maxResults: 5,
+		key: KEY,
+	},
+});
