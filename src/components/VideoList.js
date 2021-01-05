d7f3771b91af934d3072aebc3c937455cb063012
scottdavenport <scott@pinpoint.com>
2021-01-05 12:42:33 -0500

    Rendering a List of Videos

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
-	// props.video
-	return <div>{props.videos.length}</div>;
+const VideoList = ({ videos }) => {
+	// need to iterate over the videos map
+	// then need to assign the function to a const
+	// should be descriptive of what is happening
+
+	const renderedList = videos.map((video) => {
+		return <VideoItem key={video.id.videoId} />;
+	});
+
+	return <div>{renderedList}</div>;
 };
 
 export default VideoList;
