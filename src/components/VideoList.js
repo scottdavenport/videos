22b7db45c9fa5cd0eeb729f17e2a8afc38f8a0bf
scottdavenport <scott@pinpoint.com>
2021-01-05 16:58:39 -0500

    Deeply Nested Callbacks

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
 	// need to iterate over the videos map
 	// then need to assign the function to a const
 	// should be descriptive of what is happening
 
 	const renderedList = videos.map((video) => {
-		return <VideoItem video={video} />;
+		return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
 	});
 
 	return <div className="ui relaxed divided list">{renderedList}</div>;
