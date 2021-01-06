fd5a14755d104738adce9c1a37e91abd1b46141a
scottdavenport <scott@pinpoint.com>
2021-01-05 22:05:43 -0500

    Display a Video Player with iframe embed

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -7,7 +7,7 @@ const VideoList = ({ videos, onVideoSelect }) => {
 	// should be descriptive of what is happening
 
 	const renderedList = videos.map((video) => {
-		return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+		return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />;
 	});
 
 	return <div className="ui relaxed divided list">{renderedList}</div>;
